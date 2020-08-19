5b502a9564130b558fa98f97b83e538c1ce38cd3
Sergey Sharov <sssharov@domclick.ru>
2020-08-19 21:59:49 +0300

    fixes

--- functions/repo.js
+++ functions/repo.js
@@ -3,8 +3,11 @@ const { getRepoInfo } = require('../src/server/reposInfo');
 exports.handler = async function(event, context) {
 	const { owner, name } = event.queryStringParameters;
 	if (owner && name) {
-		return getRepoInfo(owner, name);
+		return await getRepoInfo(owner, name);
 	} else {
-		res.status(400).send('Request must have "owner" and "name" params');
+		return {
+			statusCode: 400,
+			body: 'Request must have "owner" and "name" params',
+		};
 	}
 }
--- functions/repos.js
+++ functions/repos.js
@@ -3,8 +3,11 @@ const { getReposInfo } = require('../src/server/reposInfo');
 exports.handler = async function(event, context) {
 	const { repos } = event.queryStringParameters;
 	if (repos) {
-		return getReposInfo(repos);
+		return await getReposInfo(repos);
 	} else {
-		res.status(400).send('Request must have "repos" param');
+		return {
+			statusCode: 400,
+			body: 'Request must have "repos" param',
+		};
 	}
 }
